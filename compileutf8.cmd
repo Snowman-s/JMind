6da5b7b62e1bfe7dc207751888e105956af8c317
Snowman-s <31811@toyota.kosen-ac.jp>
2019-12-06 22:34:46 +0900

    Integer.intValueを一本に統合

--- compileutf8.cmd
+++ compileutf8.cmd
@@ -6,8 +6,10 @@ cd ..\word\
 schange dynamic.src
 schange calc.src
 schange inspect_character.src
+schange integer.src
 cd ..
 schange compiler.src
+schange functiontype.src
 schange opecode.src
 schange utf8rap.src
 
