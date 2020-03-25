bb5e2700cc0bf435117c54cedc53545da6454678
Snowman-s <31811@toyota.kosen-ac.jp>
2020-03-25 14:42:28 +0900

    サンプル追加

--- sample/sampleloader.cmd
+++ sample/sampleloader.cmd
@@ -2,8 +2,10 @@
 ..\jmindc.exe EndEx.src
 ..\jmindc.exe HelloWorld.src
 ..\jmindc.exe If.src
+..\jmindc.exe InputSum.src
 ..\jmindc.exe For.src
 ..\jmindc.exe FizzBuzz.src
 ..\jmindc.exe StackOperate.src
+..\jmindc.exe Declare.src
 ..\jmindc.exe MWithJava.src
 javac -encoding UTF8 JWithJava.java
