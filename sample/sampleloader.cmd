6ace64269951508b5bf68d0335228b9105511831
Snowman-s <31811@toyota.kosen-ac.jp>
2020-03-26 18:52:45 +0900

    sampleloader修正

--- sample/sampleloader.cmd
+++ sample/sampleloader.cmd
@@ -1,12 +1,3 @@
 @ECHO OFF
-..\jmindc.exe EndEx.src
-..\jmindc.exe HelloWorld.src
-..\jmindc.exe If.src
-..\jmindc.exe InputSum.src
-..\jmindc.exe For.src
-..\jmindc.exe FizzBuzz.src
-..\jmindc.exe FizzBuzz2.src
-..\jmindc.exe StackOperate.src
-..\jmindc.exe Declare.src
-..\jmindc.exe MWithJava.src
-javac -encoding UTF8 JWithJava.java
+for %%a in (*.src) do ..\jmindc.exe %%a
+for %%a in (*.java) do javac -encoding UTF8 %%a
